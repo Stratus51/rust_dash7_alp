92b6e06485214fa4069bb4c1ab7cd4ae07708814
Igor Valet <igor_valet@hotmail.com>
2020-12-19 23:39:21 +0100

    [varint] Fix missing method name change

--- src/varint.rs
+++ src/varint.rs
@@ -48,7 +48,7 @@ impl Varint {
     ///
     /// The internal value is inaccessible to prevent unchecked modifications
     /// that would result in an invalid value.
-    pub const fn value(&self) -> u32 {
+    pub const fn get(&self) -> u32 {
         self.value
     }
 
