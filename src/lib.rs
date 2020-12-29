fae3fa3c066fba2738863c52984039d2cda73c21
Igor Valet <igor_valet@hotmail.com>
2020-12-29 21:47:59 +0100

    Remove outdated TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -57,8 +57,6 @@ pub mod varint;
 
 // TODO Verify each item's name against the SPEC
 
-// TODO Look into const function to replace some macros?
-// TODO Use uninitialized memory where possible
 // TODO Int enums: fn from(): find a way to avoid double value definition
 // TODO Int enums: optim: find a way to cast from int to enum instead of calling a matching
 // function (much more resource intensive). Only do that for enums that match all possible
@@ -68,9 +66,3 @@ pub mod varint;
 // troubles if we were to convert u32 to usize (even if a 64Ko payload seems a bit big).
 // Maybe we should just embrace this limitation? (Not to be lazy or anything...)
 // The bad thing is that u32 to u16 will compile and panic at runtime if the value is too big.
-// TODO Slice copies still check length consistency dynamically. Is there a way to get rid of that
-// at runtime while still testing it at compile/test time?
-//      - For simple index access, get_unchecked_mut can do the trick. But It makes the code hard to
-//      read...
-// TODO is {out = &out[offset..]; out[..size]} more efficient than {out[offset..offset+size]} ?
-// TODO Add function to encode without having to define a temporary structure
